ffe5a2596cc2ab4e9f786b7be0ccbb3482135fa9
trevorius <trevorius45@hotmail.com>
2020-11-15 11:00:43 -0500

    factintervention  type

---
+++ app/graphql/types/factintervention_type.rb
@@ -0,0 +1,17 @@
+module Types
+    class FactinterventionType < Types::BaseObject
+        field :description, String, null: true
+        field :id, ID, null: false
+        field :start_date_intervention, String, null: false
+        field :employee_id, ID, null: false
+        field :building_id, ID, null: false
+        field :battery_id, ID, null: true
+        field :column_id, ID, null: true
+        field :elevator_id, ID, null: true
+        field :end_date_intervention, String, null: true
+        field :result, String, null: false
+        field :report, String, null: true
+        field :status, String, null: false
+
+    end
+end
